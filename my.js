e92745fca36299e6125074ba9aa1ba149f4036b3
Muhammed Nihal mc <116454304+nihalmc@users.noreply.github.com>
2022-11-23 22:00:51 +0530

    yes
    ok

---
+++ my.js
@@ -0,0 +1,12 @@
+<div class="para-1">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus doloremque veniam corrupti ex optio ipsum quis aperiam inventore quae? Alias in dicta aut! Dolore deleniti eveniet maxime maiores non fugit?0</p>
+<div class="detile-s">
+   <div class="a">
+      <ul>
+        <li>Name:Nihal mc</li>
+        <li>Age:18</li>
+        <li>Occupation:Web Developer</li>
+      </ul>
+   </div>
+</div>
+</div>  
